a08433464ab559636f8ff0b2009175afedd033e7
vediicaa <100271946+vediicaa@users.noreply.github.com>
2023-10-30 00:31:10 +0530

    fifth commit

--- server/index.js
+++ server/index.js
@@ -25,12 +25,6 @@ connection();
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cors());
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
 
 // routes
 app.use("/api/user", userRoutes);
