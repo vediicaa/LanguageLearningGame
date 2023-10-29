25ff2359e593668c2a19b41e1e9b44c4089d54b8
vediicaa <100271946+vediicaa@users.noreply.github.com>
2023-10-30 00:54:14 +0530

    seventh commit

--- server/index.js
+++ server/index.js
@@ -21,12 +21,12 @@ const morgan = require('morgan');
 connection();
 
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
-  
+
 // middlewares
 app.use(morgan('tiny'));
 app.use(express.json());
